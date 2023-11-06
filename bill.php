7f8655be33c8e156447159af4aae78a8bda24617
sandra santhosh <147963290+sandra202002@users.noreply.github.com>
2023-11-06 12:04:12 +0530

    Add files via upload

---
+++ bill.php
@@ -0,0 +1,70 @@
+<html>
+    <head>
+        <title>
+            billing invoice
+        </title>
+    </head>
+    <body>
+        <table border=solid align='center'>
+            <tr>
+                <th colspan="3"><center><h3>RKS SUPERMARKET</h3></center><br></th>
+            </tr>
+            <tr>
+                <th colspan="3"><center><h5>1-01-2023</center></h5></th>
+            </tr>
+            <tr>
+
+                <th>
+                    Item name
+                </th>
+                <th>
+                    Quantity
+                </th>
+                <th>
+                    unit Price
+                </th>
+                
+            </tr>
+            <tr>
+ 
+                <th><input type='text' name='item1' placeholder="item1"></th>
+                <th><input type='number' name='qty1' placeholder="qty1"></th>
+                <th><input type='number' name='up1' placeholder="unit price"></th>
+
+            </tr>
+            <tr>
+
+                <th><input type='text' name='item2' placeholder="item1"></th>
+                <th><input type='number' name='qty2' placeholder="qty2"></th>
+                <th><input type='number' name='up2' placeholder="unit price"></th>
+>
+            </tr>
+            <tr>
+
+                <th><input type='text' name='item3' placeholder="item1"></th>
+                <th><input type='number' name='qty3' placeholder="qty3"></th>
+                <th><input type='number' name='up3' placeholder="unit price"></th>
+
+            </tr>
+            <tr>
+
+                <th><input type='text' name='item4' placeholder="item1"></th>
+                <th><input type='number' name='qty4' placeholder="qty4"></th>
+                <th><input type='number' name='up4' placeholder="unit price"></th>
+ 
+            </tr>
+            <tr>
+ 
+                <th><input type='text' name='item5' placeholder="item1"></th>
+                <th><input type='number' name='qty5' placeholder="qty5"></th>
+                <th><input type='number' name='up5' placeholder="unit price"></th>
+            </tr>
+
+          
+            <tr>
+                <th><input type="submit" name="invoice" value="sumbit"></th>
+            </tr>
+        </table>
+    </body>
+</html>
+
