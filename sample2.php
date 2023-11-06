7fc625542ddfc23d641baa6af13718daf5f621f1
sandra santhosh <147963290+sandra202002@users.noreply.github.com>
2023-11-06 10:13:48 +0530

    Add files via upload

---
+++ sample2.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    
+    <body>
+        <?php
+        
+        $t=10;
+        for($i=0;$i<=$t;$i++)
+        {
+            for($j=$i;$j<=$t;$j++)
+            {
+                echo $j;
+            }
+            echo '<br>';
+        }
+        
+        ?>
+    </body>
+</html>
+
