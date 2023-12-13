3aa613f8b3c6b371787ef2b1e0ba573e8ea53e20
sandra santhosh <147963290+sandra202002@users.noreply.github.com>
2023-12-13 11:52:33 +0530

    Update updatedb.php

--- php database/marks/updatedb.php
+++ php database/marks/updatedb.php
@@ -6,8 +6,9 @@ if($connection->connect_error)
 {
     die("connection failed" .$connection->connect_error);
 }
-$sql = "UPDATE `student_mark` SET `PASS/FAIL`='Pass' WHERE `mark`>'35' ";
-$sql = "UPDATE `student_mark` SET `PASS/FAIL`='Fail' WHERE `mark`<='35' ";
+//$sql = "UPDATE `student_mark` SET `PASS/FAIL`='Pass' WHERE `mark`>'35' ";
+//$sql = "UPDATE `student_mark` SET `PASS/FAIL`='Fail' WHERE `mark`<='35' ";
+$sql = "UPDATE `student_mark` SET  `PASS/FAIL`= CASE WHEN  mark>=35 THEN 'Pass' ELSE 'Fail' END";
 if($connection->query($sql)==TRUE)
 {
     echo "connection succesful,table student_mark is created";
